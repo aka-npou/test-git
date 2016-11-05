2fcb48ad882c14bf89cc6e3b0488de6e75ae621f
aka-npou <npou@mail.ru>
2016-11-05 11:03:08 +0300

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by npou on 05.11.2016.
+ */
+@SuppressWarnings("DefaultFileTemplate")
+class Main {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
